45fcf0f0e3491fc2f9f66578dab0992fd0f0ab37
Jethro Jorolan <rjeth21@gmail.com>
2019-07-03 16:44:57 +0800

    bootstrap implementation

---
+++ dist/js/books.js
@@ -0,0 +1,92 @@
+
+
+    $(document).ready(function () {
+
+        var array = [
+            'Genesis',
+            'Exodus',
+            'Leviticus',
+            'Numbers',
+            'Deuteronomy',
+            'Joshua',
+            'Judges',
+            'Ruth',
+            '1 Samuel',
+            '2 Samuel',
+            '1 Kings',
+            '2 Kings',
+            '1 Chronicles',
+            '2 Chronicles',
+            'Ezra',
+            'Nehemiah',
+            'Esther',
+            'Job',
+            'Psalm',
+            'Proverbs',
+            'Ecclesiastes',
+            'Song of Solomon',
+            'Isaiah',
+            'Jeremiah',
+            'Lamentations',
+            'Ezekiel',
+            'Daniel',
+            'Hosea',
+            'Joel',
+            'Amos',
+            'Obadiah',
+            'Jonah',
+            'Micah',
+            'Nahum',
+            'Habakkuk',
+            'Zephaniah',
+            'Haggai',
+            'Zechariah',
+            'Malachi',
+        ];
+        var li_books = '';
+
+        for (var i = 0; i < array.length; i++) {
+            li_books += '<li><a href="' + array[i] + '.html">' + array[i] +
+                '</li>';
+        }
+        $(".books_1").append(li_books);
+
+
+        var array = [
+            'Matthew',
+            'Mark',
+            'Luke',
+            'John',
+            'Acts',
+            'Romans',
+            '1 Corinthians',
+            '2 Corinthians',
+            'Galatians',
+            'Ephesians',
+            'Philippians',
+            'Colossians',
+            '1 Thessalonians',
+            '2 Thessalonians',
+            '1 Timothy',
+            '2 Timothy',
+            'Titus',
+            'Philemon',
+            'Hebrews',
+            'James',
+            '1 Peter',
+            '2 Peter',
+            '1 John',
+            '2 John',
+            '3 John',
+            'Jude',
+            'Revelation'
+        ];
+        var li_books1 = '';
+
+        for (var i = 0; i < array.length; i++) {
+            li_books1 += '<li><a href="' + array[i] + '.html">' + array[i] +
+                '</li>';
+        }
+        $(".books_2").append(li_books1);
+
+    });
