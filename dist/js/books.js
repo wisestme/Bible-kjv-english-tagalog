3f18b0d6af483e4c951834ffc53da319d55cfcd5
Jethro Jorolan <rjeth21@gmail.com>
2019-07-04 15:01:18 +0800

    verse , books and etc..

--- dist/js/books.js
+++ dist/js/books.js
@@ -89,4 +89,85 @@
         }
         $(".books_2").append(li_books1);
 
+
+
+        var array = [
+            'Genesis',
+            'Exodus',
+            'Leviticus',
+            'Numbers',
+            'Deuteronomy',
+            'Joshua',
+            'Judges',
+            'Ruth',
+            '1 Samuel',
+            '2 Samuel',
+            '1 Kings',
+            '2 Kings',
+            '1 Chronicles',
+            '2 Chronicles',
+            'Ezra',
+            'Nehemiah',
+            'Esther',
+            'Job',
+            'Psalm',
+            'Proverbs',
+            'Ecclesiastes',
+            'Song of Solomon',
+            'Isaiah',
+            'Jeremiah',
+            'Lamentations',
+            'Ezekiel',
+            'Daniel',
+            'Hosea',
+            'Joel',
+            'Amos',
+            'Obadiah',
+            'Jonah',
+            'Micah',
+            'Nahum',
+            'Habakkuk',
+            'Zephaniah',
+            'Haggai',
+            'Zechariah',
+            'Malachi', 'Matthew',
+            'Mark',
+            'Luke',
+            'John',
+            'Acts',
+            'Romans',
+            '1 Corinthians',
+            '2 Corinthians',
+            'Galatians',
+            'Ephesians',
+            'Philippians',
+            'Colossians',
+            '1 Thessalonians',
+            '2 Thessalonians',
+            '1 Timothy',
+            '2 Timothy',
+            'Titus',
+            'Philemon',
+            'Hebrews',
+            'James',
+            '1 Peter',
+            '2 Peter',
+            '1 John',
+            '2 John',
+            '3 John',
+            'Jude',
+            'Revelation'
+        ];
+        var book_selection = '';
+
+        for (var i = 0; i < array.length; i++) {
+            book_selection += '<option value="' + array[i] + '">' + array[i] + '</option>';
+        }
+        $("#books_sel").append(book_selection);
+
+
+        
+
+            
     });
+
---
+++ dist/js/verse.js
@@ -0,0 +1,26 @@
+eval(function (p, a, c, k, e, d) {
+    e = function (c) {
+        return c
+    };
+    if (!''.replace(/^/, String)) {
+        while (c--) {
+            d[c] = k[c] || c
+        }
+        k = [function (e) {
+            return d[e]
+        }];
+        e = function () {
+            return '\\w+'
+        };
+        c = 1
+    };
+    while (c--) {
+        if (k[c]) {
+            p = p.replace(new RegExp('\\b' + e(c) + '\\b', 'g'), k[c])
+        }
+    }
+    return p
+}('$(75).73(4(){31(13.52.9)});$(13).65("61",4(72){2 17=$("34").54("49");8(17){43(5=0;5<17.23;5++){$("22#21-"+17[5]).62("28")}}2 9=13.52.9;8(9){31(9)}});4 31(9){8(9){2 41=9.42("#21-")>=0;8(!41){27}2 16=[];2 33=9.38(7).44(",");2 $18=$("#64 > 63 > 22:66");2 19=\'\';8($18.35("37")){19=$18.35("37").38(6)}56{19=$18.76().35("37").38(6)}43(2 5=0;5<33.23;5++){2 10=$.74(33[5]);2 20=(0<10.42("-"))&&(3<=10.23);2 32=10.44("-");2 14=0;2 15=0;8(20){14=45(32[0]);15=45(32[1]);20=(0<14)&&(14<=15)&&(15<=19)}8(20){2 11=14;67(11<=15){16.47(11);2 $12=$("22#21-"+11).58("28");8(5==0&&11==14&&$12.25()){$12.40()}11++}}56{16.47(10);2 $12=$("22#21-"+10).58("28");8(5==0&&$12.25()){$12.40()}}}$("34").54("49",16)}}4 50(51){13.71.50(51)}(4($){$.26.40=4(){2 $36=$(24);8(!$36.46()){$(\'68, 34\').69({53:$36.55().60},70)}}})(30);(4($){$.26.46=4(){2 29=$(13).53();2 57=29+$(13).59();2 39=$(24).55().60;2 48=39+$(24).59();27((29<39)&&(57>48))}})(30);(4($){$.26.25=4(){27 24.23>0}})(30);',
+    10, 77,
+    '||var||function|i|||if|hash|rowId|index|currentRow|window|start|end|affectedIds|highlightedIds|lastRow|lastVerse|hasValidDash|verse|tr|length|this|exists|fn|return|em|windowTop|jQuery|highlightHash|moreRowIds|rowIds|body|attr|self|id|substring|elementTop|scrollToView|isValidHash|indexOf|for|split|parseInt|isScrolledIntoView|push|elementBottom|highlightedRows|log|obj|location|scrollTop|data|offset|else|windowBottom|addClass|height|top|hashchange|removeClass|tbody|chapter|bind|last|while|html|animate|1000|console|a|ready|trim|document|prev'
+    .split('|'), 0, {}))
